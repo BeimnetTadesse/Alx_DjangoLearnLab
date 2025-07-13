cf2cb73a86afa629766d98032005943618653cf4
Nina <140155579+BeimnetTadesse@users.noreply.github.com>
2025-07-13 20:32:39 +0300

    Create bookshelf app

--- Introduction_to_Django/LibraryProject/venv/bin/activate.fish
+++ Introduction_to_Django/LibraryProject/venv/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/). You cannot run it directly.
+# (https://fishshell.com/); you cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/aloha/Desktop/Alx_DjangoLearnLab/LibraryProject/venv"
+set -gx VIRTUAL_ENV "/Users/aloha/Desktop/Alx_DjangoLearnLab/Introduction_to_Django/LibraryProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
